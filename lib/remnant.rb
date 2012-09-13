09c33acabe9b9ebfb0957768dae2c31e366aa3d6
John Long <john.long@ors.sc.gov>
2012-09-13 16:29:24 -0400

    need to make sure to require files for non-dev modes

--- lib/remnant.rb
+++ lib/remnant.rb
@@ -2,6 +2,8 @@ require 'statsd'
 
 require 'remnant/base'
 require 'remnant/configuration'
+require 'remnant/discover'
 require 'remnant/rails'
+require 'remnant/version'
 
 require 'remnant/railtie' if defined?(::Rails::Railtie)
